b1a4148018c7e5c1e5565abc11f42511cd81e2a4
Aurel Feer <aurel.feer@gmail.com>
2023-02-24 14:42:35 +0100

    solve m1_1.py

--- lab01/m0_2.py
+++ lab01/m0_2.py
@@ -1,8 +1,12 @@
+from Crypto.Hash import SHA256
 
 def main():
-    encoding = "596f752063616e206465636f646521"
-    m = bytes.fromhex(encoding)
-    print(m.decode())
+    m = b'LoremipsumdolorsitametconsecteturadipiscingelitseddoeiusmodtemporincididuntutlaboreetdoloremagnaaliquaUtenimadminimveniamquisnostrudexercitationullamcolaborisnisiutaliquipexeacommodoconsequatDuisauteiruredolorinreprehenderitinvoluptatevelitessecillumdoloreeufugiatnullapariaturExcepteurs.'
+    s = [m[i : i + 16] for i in range(0, len(m), 16)]
+    last = [block[-1:] for block in s]
+    concat = b"".join(last)
+    res = SHA256.new(data=concat).hexdigest()
+    print(res)
 
 if __name__ == "__main__":
     main()
--- lab01/m1_1.py
+++ lab01/m1_1.py
@@ -1,8 +1,6 @@
-
 def main():
-    encoding = "596f752063616e206465636f646521"
-    m = bytes.fromhex(encoding)
-    print(m.decode())
+    m = "ጷ뼯쯾"
+    print(m.encode().hex())
 
 if __name__ == "__main__":
     main()
